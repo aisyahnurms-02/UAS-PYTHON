e803e6c45863a0b140621a93090eaa10165e7686
aisyahnurms-02 <aisyahnurms@students.amikom.ac.id>
2026-01-08 21:03:56 +0700

    Update uaspemogramandasar.py

--- uaspemogramandasar.py
+++ uaspemogramandasar.py
@@ -1,2 +1,18 @@
 import streamlit as st
-st.text("Hello World!!!")
+st.title("Kalkulator Pembagian")
+
+angka_input = st.text_input("Masukam angka:")
+angka_bagi = st.text_input("Masukan angka bagi :")
+if angka_input:
+  try:
+    hasil = int(angka_input) / int(angka_bagi)
+    st.success(f"Hasil: (hasil:.2f)")
+  except ZeroDivisionError:
+    st.error("Angka tidak boleh nol.")
+  except ValueError:
+    st.error("Masukan angka bulat yang  valid.")
+  except Exception as e:
+    st.error(f"Terjadi kesalahan lain: {e}")
+    
+
+
